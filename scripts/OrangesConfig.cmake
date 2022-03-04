c28015c5c00ae0a84845fc029491fc9d6301a0af
Ben Vining <ben.the.vining@gmail.com>
2022-03-04 17:16:02 -0600

    refactor: updating OrangesConfig
    Precommit-Verified: 9d0e923fa701879c83c6cf7109a43eeae5b620a25a98e2b75b6004dae446f05f

--- scripts/OrangesConfig.cmake
+++ scripts/OrangesConfig.cmake
@@ -26,6 +26,8 @@ include ("${CMAKE_CURRENT_LIST_DIR}/OrangesMacros.cmake")
 
 list (APPEND CMAKE_MODULE_PATH "${ORANGES_CMAKE_MODULE_PATH}")
 
+set (CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" CACHE INTERNAL "")
+
 include ("${CMAKE_CURRENT_LIST_DIR}/OrangesTargets.cmake")
 
 #
