0cab9a4e4b462ef0b7397908460928d869a7aba8
Ben Vining <ben.the.vining@gmail.com>
2022-03-19 05:42:12 -0500

    refactor: refactoring shell abstraction modules to report info using properties
    Precommit-Verified: 987d7e64069c815333fd9d68d5be8446a5eda4d4b10c691a80fa353f4a84b0a9

--- modules/shells/OrangesFindUnixShell.cmake
+++ modules/shells/OrangesFindUnixShell.cmake
@@ -17,9 +17,6 @@ Attempts to locate a UNIX shell in approximate order of preference.
 Targets:
 - Oranges::UnixShell : the located shell executable
 
-Output variables:
-- UnixShellCommand : a command line that includes any specified shell-specific flags that can be used as the COMMAND in add_custom_target, etc
-
 Supported shells (in searching order):
 - bash
 - zsh
@@ -35,14 +32,78 @@ include_guard (GLOBAL)
 
 cmake_minimum_required (VERSION 3.21 FATAL_ERROR)
 
+include (OrangesShellTargetProperties)
+
+#
+
+define_property (GLOBAL PROPERTY UNIX_SHELL_NAME BRIEF_DOCS "Name of the UNIX shell being used"
+				 FULL_DOCS "Identifier of the UNIX shell executable being used")
+
+define_property (
+	GLOBAL PROPERTY UNIX_SHELL_STARTUP_SCRIPT
+	BRIEF_DOCS "Startup script for the UNIX shell being used"
+	FULL_DOCS "Full path to a startup script for the current UNIX shell, if one is available")
+
+define_property (
+	GLOBAL PROPERTY UNIX_SHELL_LOG_FILE BRIEF_DOCS "Log file for the UNIX shell being used"
+	FULL_DOCS "Full path to a log file for the current UNIX shell, if one is available")
+
+define_property (
+	GLOBAL
+	PROPERTY UNIX_SHELL_FLAGS
+	BRIEF_DOCS "Shell-specific flags for the UNIX shell being used"
+	FULL_DOCS
+		"List of shell-specific flags to be used with the current UNIX shell executable, if any such flags have been set"
+	)
+
+define_property (
+	GLOBAL
+	PROPERTY UNIX_SHELL_COMMAND
+	BRIEF_DOCS "UNIX shell command line"
+	FULL_DOCS
+		"A UNIX shell command line that includes any specified shell-specific flags and can be used as the COMMAND in add_custom_target, etc"
+	)
+
+#
+
+function(_oranges_set_unix_shell_global_properties)
+
+	if(NOT TARGET Oranges::UnixShell)
+		return ()
+	endif()
+
+	get_target_property (shell_name Oranges::UnixShell SHELL_NAME)
+	set_property (GLOBAL PROPERTY UNIX_SHELL_NAME "${shell_name}")
+
+	get_target_property (startup_script Oranges::UnixShell SHELL_STARTUP_SCRIPT)
+	if(startup_script)
+		set_property (GLOBAL PROPERTY UNIX_SHELL_STARTUP_SCRIPT "${startup_script}")
+	endif()
+
+	get_target_property (log_file Oranges::UnixShell SHELL_LOG_FILE)
+	if(log_file)
+		set_property (GLOBAL PROPERTY UNIX_SHELL_LOG_FILE "${log_file}")
+	endif()
+
+	get_target_property (shell_flags Oranges::UnixShell SHELL_FLAGS)
+	if(shell_flags)
+		set_property (GLOBAL PROPERTY UNIX_SHELL_FLAGS "${shell_flags}")
+	endif()
+
+	set_property (GLOBAL PROPERTY UNIX_SHELL_COMMAND Oranges::UnixShell ${shell_flags})
+
+endfunction()
+
+cmake_language (DEFER CALL _oranges_set_unix_shell_global_properties)
+
+#
+
 # bash
 
 find_package (Bash)
 
 if(TARGET Bash::Bash)
 	add_executable (Oranges::UnixShell ALIAS Bash::Bash)
-
-	set (UnixShellCommand Oranges::UnixShell ${BASH_FLAGS})
 
 	return ()
 endif()
@@ -54,8 +115,6 @@ find_package (zsh)
 if(TARGET zsh::zsh)
 	add_executable (Oranges::UnixShell ALIAS zsh::zsh)
 
-	set (UnixShellCommand Oranges::UnixShell ${ZSH_FLAGS})
-
 	return ()
 endif()
 
@@ -65,8 +124,6 @@ find_package (fish)
 
 if(TARGET fish::fish)
 	add_executable (Oranges::UnixShell ALIAS fish::fish)
-
-	set (UnixShellCommand Oranges::UnixShell ${FISH_FLAGS})
 
 	return ()
 endif()
@@ -78,8 +135,6 @@ find_package (ion)
 if(TARGET ion::ion)
 	add_executable (Oranges::UnixShell ALIAS ion::ion)
 
-	set (UnixShellCommand Oranges::UnixShell ${ION_FLAGS})
-
 	return ()
 endif()
 
@@ -89,8 +144,6 @@ find_package (ksh)
 
 if(TARGET ksh::ksh)
 	add_executable (Oranges::UnixShell ALIAS ksh::ksh)
-
-	set (UnixShellCommand Oranges::UnixShell ${KSH_FLAGS})
 
 	return ()
 endif()
@@ -102,8 +155,6 @@ find_package (csh)
 if(TARGET csh::csh)
 	add_executable (Oranges::UnixShell ALIAS csh::csh)
 
-	set (UnixShellCommand Oranges::UnixShell ${CSH_FLAGS})
-
 	return ()
 endif()
 
@@ -113,8 +164,6 @@ find_package (sh)
 
 if(TARGET sh::sh)
 	add_executable (Oranges::UnixShell ALIAS sh::sh)
-
-	set (UnixShellCommand Oranges::UnixShell ${SH_FLAGS})
 
 	return ()
 endif()
--- modules/shells/OrangesFindWindowsShell.cmake
+++ modules/shells/OrangesFindWindowsShell.cmake
@@ -32,14 +32,77 @@ cmake_minimum_required (VERSION 3.21 FATAL_ERROR)
 
 include (OrangesShellTargetProperties)
 
+#
+
+define_property (
+	GLOBAL PROPERTY WINDOWS_SHELL_NAME BRIEF_DOCS "Name of the Windows shell being used"
+	FULL_DOCS "Identifier of the Windows shell executable being used")
+
+define_property (
+	GLOBAL PROPERTY WINDOWS_SHELL_STARTUP_SCRIPT
+	BRIEF_DOCS "Startup script for the Windows shell being used"
+	FULL_DOCS "Full path to a startup script for the current Windows shell, if one is available")
+
+define_property (
+	GLOBAL PROPERTY WINDOWS_SHELL_LOG_FILE BRIEF_DOCS "Log file for the Windows shell being used"
+	FULL_DOCS "Full path to a log file for the current Windows shell, if one is available")
+
+define_property (
+	GLOBAL
+	PROPERTY WINDOWS_SHELL_FLAGS
+	BRIEF_DOCS "Shell-specific flags for the Windows shell being used"
+	FULL_DOCS
+		"List of shell-specific flags to be used with the current Windows shell executable, if any such flags have been set"
+	)
+
+define_property (
+	GLOBAL
+	PROPERTY WINDOWS_SHELL_COMMAND
+	BRIEF_DOCS "Windows shell command line"
+	FULL_DOCS
+		"A Windows shell command line that includes any specified shell-specific flags and can be used as the COMMAND in add_custom_target, etc"
+	)
+
+#
+
+function(_oranges_set_windows_shell_global_properties)
+
+	if(NOT TARGET Oranges::WindowsShell)
+		return ()
+	endif()
+
+	get_target_property (shell_name Oranges::WindowsShell SHELL_NAME)
+	set_property (GLOBAL PROPERTY WINDOWS_SHELL_NAME "${shell_name}")
+
+	get_target_property (startup_script Oranges::WindowsShell SHELL_STARTUP_SCRIPT)
+	if(startup_script)
+		set_property (GLOBAL PROPERTY WINDOWS_SHELL_STARTUP_SCRIPT "${startup_script}")
+	endif()
+
+	get_target_property (log_file Oranges::WindowsShell SHELL_LOG_FILE)
+	if(log_file)
+		set_property (GLOBAL PROPERTY WINDOWS_SHELL_LOG_FILE "${log_file}")
+	endif()
+
+	get_target_property (shell_flags Oranges::WindowsShell SHELL_FLAGS)
+	if(shell_flags)
+		set_property (GLOBAL PROPERTY WINDOWS_SHELL_FLAGS "${shell_flags}")
+	endif()
+
+	set_property (GLOBAL PROPERTY WINDOWS_SHELL_COMMAND Oranges::WindowsShell ${shell_flags})
+
+endfunction()
+
+cmake_language (DEFER CALL _oranges_set_windows_shell_global_properties)
+
+#
+
 # PowerShell
 
 find_package (PowerShell)
 
 if(TARGET PowerShell::PowerShell)
 	add_executable (Oranges::WindowsShell ALIAS PowerShell::PowerShell)
-
-	set (WindowsShellCommand Oranges::WindowsShell ${POWERSHELL_FLAGS})
 
 	return ()
 endif()
@@ -50,8 +113,6 @@ find_package (cmd)
 
 if(TARGET cmd::cmd)
 	add_executable (Oranges::WindowsShell ALIAS cmd_exe)
-
-	set (WindowsShellCommand Oranges::WindowsShell ${CMD_EXE_FLAGS})
 
 	return ()
 endif()
