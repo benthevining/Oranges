35dfd306c5d3860a763af90aa5244d3ebd94e027
Ben Vining <ben.the.vining@gmail.com>
2022-03-26 17:25:24 -0500

    fix: fixing bumpversion regex
    Precommit-Verified: f1780edfb8850a7628df5b684f6c7d472090608984e4b4f3f29baaf15a2cfa0c

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import CMake, ConanFile
 
 class OrangesConan(ConanFile):
 	name = "Oranges"
-	version = "1.28.0"
+	version = "1.28.1"
 	license = "GPL3"
 	url = "https://github.com/benthevining/Oranges"
 	description = "CMake modules, scripts, and utilities"
