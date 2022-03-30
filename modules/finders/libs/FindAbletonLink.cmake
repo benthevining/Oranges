f37f876fbbb4fe82d4f8753e421d4fea7c4ce7d8
Ben Vining <ben.the.vining@gmail.com>
2022-03-30 18:17:06 -0500

    feat: added a find module for ableton link
    Precommit-Verified: 4cd1c13e6c94b04089995398d848a103acbb3d9c2c387e70d4398b9c485303ac

---
+++ modules/finders/libs/FindAbletonLink.cmake
@@ -0,0 +1,88 @@
+# ======================================================================================
+#    ____  _____            _   _  _____ ______  _____
+#   / __ \|  __ \     /\   | \ | |/ ____|  ____|/ ____|
+#  | |  | | |__) |   /  \  |  \| | |  __| |__  | (___
+#  | |  | |  _  /   / /\ \ | . ` | | |_ |  __|  \___ \
+#  | |__| | | \ \  / ____ \| |\  | |__| | |____ ____) |
+#   \____/|_|  \_\/_/    \_\_| \_|\_____|______|_____/
+#
+#  This file is part of the Oranges open source CMake library and is licensed under the terms of the GNU Public License.
+#
+# ======================================================================================
+
+#[=======================================================================[.rst:
+
+FindAbletonLink
+-------------------------
+
+A find module for Ableton's Link library.
+
+Output variables
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+- AbletonLink_FOUND
+
+Targets
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+- Ableton::Link : the Ableton Link library
+
+#]=======================================================================]
+
+cmake_minimum_required (VERSION 3.21 FATAL_ERROR)
+
+include (OrangesFindPackageHelpers)
+
+set_package_properties (
+	AbletonLink PROPERTIES URL "https://www.ableton.com/en/link/"
+	DESCRIPTION "Library for communicating tempo changes between devices in a session")
+
+#
+
+oranges_file_scoped_message_context ("FindAbletonLink")
+
+set (AbletonLink_FOUND FALSE)
+
+#
+
+find_package_try_pkgconfig (Ableton::Link)
+
+#
+
+find_path (ABLETONLINK_INCLUDES fftw3.h)
+
+find_library (ABLETONLINK_LIBRARIES NAMES fftw3)
+
+mark_as_advanced (FORCE ABLETONLINK_INCLUDES ABLETONLINK_LIBRARIES)
+
+if(ABLETONLINK_INCLUDES AND ABLETONLINK_LIBRARIES)
+	add_library (AbletonLink IMPORTED UNKNOWN)
+
+	set_target_properties (AbletonLink PROPERTIES IMPORTED_LOCATION "${ABLETONLINK_LIBRARIES}")
+
+	target_include_directories (AbletonLink PUBLIC "${ABLETONLINK_INCLUDES}")
+
+	add_library (Ableton::Link ALIAS AbletonLink)
+
+	set (AbletonLink_FOUND TRUE)
+
+	return ()
+endif()
+
+include (OrangesFetchRepository)
+
+if(AbletonLink_FIND_QUIETLY)
+	set (quiet_flag QUIET)
+endif()
+
+oranges_fetch_repository (
+	NAME
+	AbletonLink
+	GITHUB_REPOSITORY
+	Ableton/link
+	GIT_TAG
+	origin/master
+	NEVER_LOCAL
+	${quiet_flag})
+
+unset (quiet_flag)
+
+set (AbletonLink_FOUND TRUE)
