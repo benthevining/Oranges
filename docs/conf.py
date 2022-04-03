5177127b1e5736bef016fe2f707c3e6e5b9c409b
Ben Vining <ben.the.vining@gmail.com>
2022-04-02 23:30:54 -0500

    docs: initial commit of docs build config
    Precommit-Verified: 078fdd27e8f520805f247d6c7c59e123a3b62f971a2fdae2b6760af5fffc07d6

---
+++ docs/conf.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+# ======================================================================================
+#    ____  _____            _   _  _____ ______  _____
+#   / __ \|  __ \     /\   | \ | |/ ____|  ____|/ ____|
+#  | |  | | |__) |   /  \  |  \| | |  __| |__  | (___
+#  | |  | |  _  /   / /\ \ | . ` | | |_ |  __|  \___ \
+#  | |__| | | \ \  / ____ \| |\  | |__| | |____ ____) |
+#   \____/|_|  \_\/_/    \_\_| \_|\_____|______|_____/
+#
+#  This file is part of the Oranges open source CMake library and is licensed under the terms of the GNU Public License.
+#
+# ======================================================================================
+
+import glob
+import os
+import re
+import sys
+
+
+sys.path.insert(0, r'@conf_path@')
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = 'Oranges'
+copyright = '@conf_copyright@'  # pylint: disable=redefined-builtin
+version = '@conf_version@'  # feature version
+release = '@conf_version@'  # full version string
+pygments_style = 'colors.CMakeTemplateStyle'
+
+language = 'en'
+primary_domain = 'cmake'
+highlight_language = 'none'
+
+exclude_patterns = [
+    'dev',  # ignore developer-only documentation
+]
+
+extensions = ['cmake']
+templates_path = ['@conf_path@/templates']
+
+nitpicky = True
+smartquotes = False
+
+cmake_manuals = sorted(glob.glob(r'@conf_docs@/manual/*.rst'))
+cmake_manual_description = re.compile(r'^\.\. cmake-manual-description:(.*)$')
+man_pages = []
+
+for fpath in cmake_manuals:
+	try:
+		name, sec, rst = os.path.basename(fpath).split('.')
+		desc = None
+
+		with open(fpath, 'r') as f:
+			for l in f:
+				m = cmake_manual_description.match(l)
+				if m:
+					desc = m.group(1).strip()
+					break
+
+		if desc:
+			man_pages.append(
+			    (f'manual/{name}.{sec}', name, desc, [], int(sec)))
+		else:
+			sys.stderr.write(
+			    f"ERROR: No cmake-manual-description in '{fpath}'\n")
+	except Exception as e:  # pylint: disable=broad-except
+		sys.stderr.write(f'ERROR: {e}\n')
+
+man_show_urls = False
+man_make_section_directory = False
+
+html_show_sourcelink = True
+html_static_path = ['@conf_path@/static']
+html_style = 'cmake.css'
+html_theme = 'default'
+html_theme_options = {
+    'footerbgcolor': '#00182d',
+    'footertextcolor': '#ffffff',
+    'sidebarbgcolor': '#e4ece8',
+    'sidebarbtncolor': '#00a94f',
+    'sidebartextcolor': '#333333',
+    'sidebarlinkcolor': '#00a94f',
+    'relbarbgcolor': '#00529b',
+    'relbartextcolor': '#ffffff',
+    'relbarlinkcolor': '#ffffff',
+    'bgcolor': '#ffffff',
+    'textcolor': '#444444',
+    'headbgcolor': '#f2f2f2',
+    'headtextcolor': '#003564',
+    'headlinkcolor': '#3d8ff2',
+    'linkcolor': '#2b63a8',
+    'visitedlinkcolor': '#2b63a8',
+    'codebgcolor': '#eeeeee',
+    'codetextcolor': '#333333',
+}
+
+html_title = f'Oranges {release} Documentation'
+html_short_title = f'{release} Documentation'
+#html_favicon = '@conf_path@/static/cmake-favicon.ico'
+
+# Not supported yet by sphinx:
+# https://bitbucket.org/birkenfeld/sphinx/issue/1448/make-qthelp-more-configurable
+# qthelp_namespace = "org.cmake"
+# qthelp_qch_name = "CMake.qch"
