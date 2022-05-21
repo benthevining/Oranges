4e4ebec539203b6941b5fee1c69fbde700b4b9f1
Ben Vining <ben.the.vining@gmail.com>
2022-05-20 22:32:19 -0500

    refactor: updating find_package version bump script
    Precommit-Verified: bcf7d5df4506b003b0e64bd90573e1e5cd51619013d4e5f311b97e8e849f513f

--- scripts/update_find_package_version.py
+++ scripts/update_find_package_version.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This script searches a project source tree recursively for any occurrences of a CMake find_package() command for a specified package, and updates the
-version used in the find_package() command to a new one specified.
+This script searches a project source tree recursively for any occurrences of a CMake find_package() or find_dependency() command for a specified
+package, and updates the version used in the find_package() command to a new one specified.
 """
 
 # ======================================================================================
@@ -23,16 +23,16 @@ import os
 #
 
 
-def process_file(package_name: str, new_version: str, file_path: str):
+def process_file(package_name: str, new_version: str, file_path: str) -> bool:
 	"""
 	Processes a single CMake file.
 	"""
 
 	# pylint: disable=too-many-locals
 
-	if not (os.path.splitext(file_path)[1]
-	        == ".cmake") or (os.path.basename(file_path) == "CMakeLists.txt"):
-		return
+	if not (os.path.splitext(file_path)[1] == ".cmake"
+	        or os.path.basename(file_path) == "CMakeLists.txt"):
+		return False
 
 	with open(file_path, "r", encoding="utf-8") as file_in:
 		file_lines: list[str] = file_in.readlines()
@@ -43,7 +43,9 @@ def process_file(package_name: str, new_version: str, file_path: str):
 		stripped_line: Final[str] = line.strip()
 
 		if not (stripped_line.startswith("find_package")
-		        or stripped_line.startswith("FIND_PACKAGE")):
+		        or stripped_line.startswith("FIND_PACKAGE")
+		        or stripped_line.startswith("find_dependency")
+		        or stripped_line.startswith("FIND_DEPENDENCY")):
 			continue
 
 		# this is another command that starts with find_package_
@@ -51,7 +53,7 @@ def process_file(package_name: str, new_version: str, file_path: str):
 			continue
 
 		find_pkg_args_string: Final[str] = stripped_line.split("(",
-		                                                       1)[1].split(
+		                                                       1)[-1].split(
 		                                                           ")", 1)[0]
 
 		del stripped_line
@@ -80,45 +82,60 @@ def process_file(package_name: str, new_version: str, file_path: str):
 		del find_pkg_version
 
 		# either replace the given version, or insert it and scoot other args back
-
 		if version_was_given:
 			find_pkg_args[1] = new_version
 		else:
 			find_pkg_args.insert(1, new_version)
 
-		new_find_pkg_args_str: Final[str] = " ".join(find_pkg_args)
-
-		del find_pkg_args
-
-		file_lines[
-		    idx] = orig_chars_before_find_pkg_args + new_find_pkg_args_str
+		file_lines[idx] = orig_chars_before_find_pkg_args + " ".join(
+		    find_pkg_args) + ")\n"
 
 		anything_changed = True
 
-	if anything_changed:
-		with open(file_path, "w", encoding="utf-8") as file_out:
-			file_out.write("".join(file_lines))
+	if not anything_changed:
+		return False
+
+	print(f"Updating {file_path}")
+
+	with open(file_path, "w", encoding="utf-8") as file_out:
+		file_out.write("".join(file_lines))
+
+	return True
 
 
 #
 
 
-def process_directory(package_name: str, new_version: str, dir_path: str):
+# editorconfig-checker-disable
+def process_directory(package_name: str, new_version: str,
+                      dir_path: str) -> bool:
 	"""
 	Processes all CMake files in this directory recursively.
 	"""
+
+	# editorconfig-checker-enable
+
+	any_files_changed: bool = False
 
 	for entryname in os.listdir(dir_path):
 		entry_path: Final[str] = os.path.join(dir_path, entryname)
 
 		if os.path.isdir(entry_path):
-			process_directory(package_name=package_name,
-			                  new_version=new_version,
-			                  dir_path=entry_path)
+			if entryname in ["Builds", "Cache"]:
+				continue
+
+			any_files_changed = process_directory(
+			    package_name=package_name,
+			    new_version=new_version,
+			    dir_path=entry_path) or any_files_changed
+
 		elif os.path.isfile(entry_path):
-			process_file(package_name=package_name,
-			             new_version=new_version,
-			             file_path=entry_path)
+			any_files_changed = process_file(
+			    package_name=package_name,
+			    new_version=new_version,
+			    file_path=entry_path) or any_files_changed
+
+	return any_files_changed
 
 
 #
@@ -126,7 +143,7 @@ def process_directory(package_name: str, new_version: str, dir_path: str):
 
 # editorconfig-checker-disable
 def update_find_package_version(package_name: str, new_version: str,
-                                root_dir: str):
+                                root_dir: str) -> bool:
 	"""
 	Scans all CMake files found in <root_dir> for occurrences of find_package(<package_name>), and updates them to use the <new_version> specified.
 	"""
@@ -134,9 +151,13 @@ def update_find_package_version(package_name: str, new_version: str,
 	if not os.path.isabs(root_dir):
 		root_dir = os.path.join(os.getcwd(), root_dir)
 
-	process_directory(package_name=package_name,
-	                  new_version=new_version,
-	                  dir_path=root_dir)
+	anything_changed: Final[bool] = process_directory(
+	    package_name=package_name, new_version=new_version, dir_path=root_dir)
+
+	if not anything_changed:
+		print("All files up to date!")
+
+	return anything_changed
 
 
 # editorconfig-checker-enable
